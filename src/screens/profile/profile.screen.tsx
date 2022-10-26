73b13f591a526c148da389d98dfe0ab0f13d11f7
Akshanth <77204659+Akshanthv@users.noreply.github.com>
2022-10-26 10:37:55 +0530

    Update profile.screen.tsx

--- src/screens/profile/profile.screen.tsx
+++ src/screens/profile/profile.screen.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProfileCard from '../../components/profile.card.component';
 import { IProfile } from '../../entities/profile.entity';
 import profileItems from './profile.items';
+import axios, { AxiosResponse } from 'axios';
+
 
 
 const Profile = () => {
+
+    const fetchProfile = () => {
+        console.log("Fetch Profile")
+        axios.get("http://localhost:8080/evesync/api/v1/users")
+            .then((response: AxiosResponse<IProfile>) => {
+                console.log("Events Data:::", response.data);
+            })
+    }
 
     return <React.Fragment>
         <span>Profile Screen</span>
